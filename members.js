4b648198d09d9068c189578b2d0797e3e92edbab
Charchit <charchitpaharia@gmail.com>
2024-06-22 23:40:28 +0530

    Initial commit with Express API project

---
+++ members.js
@@ -0,0 +1,19 @@
+const items = [
+    {
+      id: 1,
+      name: 'Shirt',
+      price: 800
+    },
+    {
+      id: 2,
+      name: 'Pant',
+      price: 1200
+    },
+    {
+      id: 3,
+      name: 'Cap',
+      price: 350
+    }
+  ];
+  
+  module.exports = items;
